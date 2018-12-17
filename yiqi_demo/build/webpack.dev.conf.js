01dd44415fa38e9925e34c04ce566e7d9088e538
yiqiwang(王一棋) <yiqiwang@tencent.com>
2018-12-17 21:37:21 +0800

    [add] Add json-server

--- yiqi_demo/build/webpack.dev.conf.js
+++ yiqi_demo/build/webpack.dev.conf.js
@@ -93,3 +93,14 @@ module.exports = new Promise((resolve, reject) => {
     }
   })
 })
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+ 
+server.use(middlewares)
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running')
+})
